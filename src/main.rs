36e96c5f95544adcc604a435b0d121e17b2ea42a
workshop748 <jacobpeacy11@gmail.com>
2024-01-18 21:41:21 -0600

    adding my first rust project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![allow(unused)]//will remove warming for things unused
+const BIRTHDAY:u32 =1;
+fn main() {
+let my_name = "Jacob";
+let my_birthday ="08/11";
+let mut age = 20;
+let new_age = age+BIRTHDAY;
+
+println!("My name is {} and i am {} years old. I will turn {} on {}",my_name,age,new_age,my_birthday);
+}
